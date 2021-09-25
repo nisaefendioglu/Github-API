8678bde829cfe84ec8537a11a512b4f2637a0077
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-09-25 13:00:16 +0300

    edited.

--- app/src/main/java/com/nisaefendioglu/github/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/github/MainActivity.java
@@ -1,14 +1,89 @@
 package com.nisaefendioglu.github;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    LinearLayout linearlayout;
+    ImageButton github,search;
+    EditText user;
+    RecyclerView recyclerview;
+
+    ApiService apiService;
+    RepoAdapter repoAdapter;
+    LinearLayoutManager linearLayoutManager;
+
+    String username;
+
+    List<Repo> repoList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        apiService = RetrofitConfig.retrofit.create(ApiService.class);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = user.getText().toString();
+
+                call().enqueue(new Callback<List<Repo>>() {
+                    @Override
+                    public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+
+                        repoList = response.body();
+
+                        repoAdapter = new RepoAdapter(MainActivity.this,repoList);
+
+                        recyclerview.setLayoutManager(linearLayoutManager);
+
+                        recyclerview.setAdapter(repoAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Repo>> call, Throwable t) {
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    void init(){
+        linearlayout = findViewById(R.id.linearlayout);
+        github = findViewById(R.id.github);
+        search = findViewById(R.id.search);
+        user = findViewById(R.id.user);
+        recyclerview = findViewById(R.id.recyclerview);
+
+    }
+
+    private Call<List<Repo>> call(){
+        return apiService.listRepos(this.username);
+
     }
 }
--- app/src/main/java/com/nisaefendioglu/github/RetrofitConfig.java
+++ app/src/main/java/com/nisaefendioglu/github/RetrofitConfig.java
@@ -1,11 +1,15 @@
 package com.nisaefendioglu.github;
 
+
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitConfig {
 
-    Retrofit retrofit = new Retrofit.Builder()
+    public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build();
 
 }
